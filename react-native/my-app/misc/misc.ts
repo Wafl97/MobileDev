ed4fec169f86ef5f3d78b6aa4d55dd8072b75a23
Wafl97 <mlwellerb@gmail.com>
2022-09-19 12:12:06 +0200

    fetching data now

--- react-native/my-app/misc/misc.ts
+++ react-native/my-app/misc/misc.ts
@@ -1,3 +1,5 @@
+// Legit env file :)
+
 const api_key = "?api_key=f39192085612fca04ed55b2c8a67faf4";
 const api_url = "https://api.themoviedb.org/3";
 const img_path = "https://image.tmdb.org/t/p/w500";

c398035b540ba43da8cef86856ecb5658fb40338
Wafl97 <mlwellerb@gmail.com>
2022-11-21 02:35:32 +0100

    pwa

---
+++ pwa/todo-app/frontend/src/config/Routes.ts
@@ -0,0 +1,15 @@
+import { lazy } from "react";
+import IRoute from "../interfaces/IRoute"
+
+//Views
+const Dashboard = lazy(() => import("../views/Dashboard"));
+
+const routes: IRoute[] = [
+    {
+        name: "dashboard",
+        path: "/",
+        view: Dashboard,
+    },
+];
+
+export default routes;

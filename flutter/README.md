b2b5cf98dd20f0e6c22012e82770429516e45f50
Wafl97 <mlwellerb@gmail.com>
2022-12-13 01:48:15 +0100

    docker fix

---
+++ flutter/README.md
@@ -0,0 +1,15 @@
+# Flutter Todo App
+
+## Run the app
+
+First start the backend:
+
+`docker-compose up -d`
+
+Then go to `/frontend` and run:
+
+`flutter pub get`
+
+`flutter run`
+
+Enjoy :)
--- flutter/todo_app/backend/service/src/middleware/Authentication.ts
+++ flutter/todo_app/backend/service/src/middleware/Authentication.ts
@@ -3,8 +3,6 @@ import ReturnObj from "../types/ReturnObj";
 import Env from "../env/Env";
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => { 
-        console.log(req.method, req.url);    
-        console.log(req.body);
         if (!req.header("api_key")) return res.status(200).json(new ReturnObj("missing api key"));
         if (req.header("api_key") !== Env.KEY) return res.status(200).json(new ReturnObj("invalid api key"));
         next();
